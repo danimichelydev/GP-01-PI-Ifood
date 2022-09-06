41a0bfa6316f2a38760b2b422a5428b80a1bf06d
Dani Michely <danielamichelydev@gmail.com>
2022-09-06 11:48:11 -0300

    Ajuste de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,23 @@
-import Router from "express";
-import userController from "./app/controllers/UserControllers_old.js";
+import Router from 'express';
+import userController from './app/controllers/UserControllers_old.js';
 import validatorUsers from './middlewares/ValidatorUser.js';
 import UserController from './app/controllers/UserController.js';
 import MotelController from './app/controllers/MotelController.js';
 
 const routes = new Router();
 
-routes.get("/users/:id", UserController.listaUmUsuario);
-routes.post("/users", UserController.addUsuario);
-routes.get("/users", UserController.listarUsuarios);
-routes.put("/users/:id", UserController.atualizaUsuario);
-routes.delete("/users/:id", UserController.deletaUsuario);
+routes.get('/users/:id', UserController.listaUmUsuario);
+routes.post('/users', UserController.addUsuario);
+routes.get('/users', UserController.listarUsuarios);
+routes.put('/users/:id', UserController.atualizaUsuario);
+routes.delete('/users/:id', UserController.deletaUsuario);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-module.exports = routes;
-// coment
-=======
-=======
->>>>>>> 1fd603135547d63796bc9c8733d31e3b88e35cf1
-routes.get("/moteis/:id", MotelController.listaUmMotel);
-routes.post("/moteis", MotelController.addMotel);
-routes.get("/moteis", MotelController.listarMoteis);
-routes.put("/moteis/:id", MotelController.atualizaMotel);
-routes.delete("/moteis/:id", MotelController.deletaMotel);
+// module.exports = routes;
 
+routes.get('/moteis/:id', MotelController.listaUmMotel);
+routes.post('/moteis', MotelController.addMotel);
+routes.get('/moteis', MotelController.listarMoteis);
+routes.put('/moteis/:id', MotelController.atualizaMotel);
+routes.delete('/moteis/:id', MotelController.deletaMotel);
 
 export { routes as default };
-<<<<<<< HEAD
->>>>>>> 94783711732cd9e2d82a29532074b1a2be524c84
-=======
->>>>>>> 1fd603135547d63796bc9c8733d31e3b88e35cf1
