ab4e53089f8a079a2fc20496c5da1bef9d02facb
genesis-g101 <gyreplicant@gmail.com>
2022-09-16 21:04:06 -0300

    renomeando as tabelas e migrations

--- src/app/models/MotelModel.js
+++ src/app/models/MotelModel.js
@@ -1,32 +1,34 @@
-import Sequelize, { Model } from "sequelize";
-import databaseConfig from "../../config/database.js";
-import EnderecoModel from "../models/EnderecoModel.js";
+import Sequelize, { Model } from 'sequelize';
+import databaseConfig from '../../config/database.js';
+import EnderecoModel from '../models/EnderecoModel.js';
 
 const sequelize = new Sequelize(databaseConfig);
 
-class MotelModel extends Model { }
+class MotelModel extends Model {}
 
-MotelModel.init({
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
+MotelModel.init(
+    {
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        nome: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        cnpj: {
+            type: Sequelize.STRING(18),
+            allowNull: false,
+        },
     },
-    nome: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-    },
-    cnpj: {
-        type: Sequelize.STRING(18),
-        allowNull: false,
-    }  
-},
     {
         sequelize,
-        modelName: "Moteis",
+        modelName: 'moteis',
         timestamps: false,
         underscored: true,
-    });
+    }
+);
 
 export default MotelModel;
--- src/app/models/UserModel.js
+++ src/app/models/UserModel.js
@@ -1,45 +1,47 @@
 // formato de criação do model
 
-import Sequelize, { Model } from "sequelize";
-import databaseConfig from "../../config/database.js";
+import Sequelize, { Model } from 'sequelize';
+import databaseConfig from '../../config/database.js';
 const sequelize = new Sequelize(databaseConfig);
 
-class UsuarioModel extends Model { }
+class UsuarioModel extends Model {}
 
-UsuarioModel.init({
-    id: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
-    nome: {
-        type: Sequelize.STRING(50),
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.STRING(150),
-        allowNull: false,
-        unique: true,
-    },
-    cpf: {
-        type: Sequelize.STRING(15),
-        allowNull: false,
-    },
-    dataNascimento: {
-        type: Sequelize.DATEONLY,
-        allowNull: false,
-    },
-    senha: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    }
+UsuarioModel.init(
+    {
+        id: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        nome: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        email: {
+            type: Sequelize.STRING(150),
+            allowNull: false,
+            unique: true,
+        },
+        cpf: {
+            type: Sequelize.STRING(15),
+            allowNull: false,
+        },
+        dataNascimento: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        senha: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
     },
     {
         sequelize,
-        modelName: "Usuarios",
+        modelName: 'usuarios',
         timestamps: false,
         underscored: true,
-    });
+    }
+);
 
 export default UsuarioModel;
