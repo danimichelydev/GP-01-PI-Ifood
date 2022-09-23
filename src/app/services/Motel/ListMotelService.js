6e3d31e963143c0da65c5449d254d9f0f1474bf2
genesis-g101 <gyreplicant@gmail.com>
2022-09-23 20:07:11 -0300

    implementando checagem das buscas de moteis

--- src/app/services/Motel/ListMotelService.js
+++ src/app/services/Motel/ListMotelService.js
@@ -2,53 +2,58 @@ import MotelModel from '../../models/MotelModel';
 import EnderecoModel from '../../models/EnderecoModel';
 
 export default class ListMotelService {
-    constructor() {}
+  constructor() {}
 
-    async listAll() {
-        try {
-            const moteis = await MotelModel.findAll( {
-                include: {
-                    model: EnderecoModel,
-                    attributes: ['bairro'],
-                    as: 'endereco',
-                }, 
-            });
-            return (moteis);
-        } catch (error) {
-            return { erro: error.message };
-        }
+  async listAll() {
+    try {
+      const moteis = await MotelModel.findAll({
+        include: {
+          model: EnderecoModel,
+          attributes: ['bairro'],
+          as: 'endereco',
+        },
+      });
+      return moteis;
+    } catch (error) {
+      return { erro: error.message };
     }
+  }
 
-    async listOne(nomeMotel) {
-        try {
-            const umMotel = await MotelModel.findOne({
-                where: { nome: nomeMotel },
-            });
+  async listOne(nomeMotel) {
+    try {
+      const umMotel = await MotelModel.findOne({
+        where: { nome: nomeMotel },
+      });
+      if (!umMotel) {
+        return { erro: 'Motel não encontrado.' };
+      }
 
-            return umMotel;
-        } catch (error) {
-            console.log(error);
-            return { erro: 'Motel não encontrado.' };
-        }
+      return umMotel;
+    } catch (error) {
+      console.log(error);
+      return { erro: error.message };
     }
+  }
 
-    async listAllByBairro(nomeBairro) {
-        try {
-            const moteis = await EnderecoModel.findAll({
-                include: {
-                    model: MotelModel,
-                    attributes: ['nome'],
-                    as: 'moteis',
-                },
+  async listAllByBairro(nomeBairro) {
+    try {
+      const moteis = await EnderecoModel.findAll({
+        include: {
+          model: MotelModel,
+          attributes: ['nome'],
+          as: 'moteis',
+        },
 
-                attributes: ['rua', 'numero'],
-                where: { bairro: nomeBairro },
-            });
-
-            return moteis;
-        } catch (error) {
-            console.log(error);
-            return { erro: 'Não há motéis cadastrados nesse bairro.' };
-        }
+        attributes: ['rua', 'numero'],
+        where: { bairro: nomeBairro },
+      });
+      if (!moteis || moteis.length < 1) {
+        return { erro: 'Não há motéis cadastrados nesse bairro.' };
+      }
+      return moteis;
+    } catch (error) {
+      console.log(error);
+      return { erro: error.message };
     }
+  }
 }
